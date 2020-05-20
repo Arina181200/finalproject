721e56b66cff4c8f1f6c0206eaae37ce1dd3b283
Arina Davydova <arina.davidova1812@mail.ru>
2020-05-20 13:39:37 +0600

    Initial commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,9 @@
+
+
+public class Runner {
+    public static void main(String[] args)
+    {
+        System.setProperty("log4j.configurationFile","C:\\Users\\22807\\finalproject\\src\\main\\resources\\log4j2.xml");
+        Logger logger = LogManager.getRootLogger();
+    }
+}
